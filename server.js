55637300fc908e579ec41f7548a51d14fce64655
heavenly-zy <1457715339@qq.com>
2019-11-23 21:44:53 +0800

    根据请求的URL路径拼出对应的文件路径

--- server.js
+++ server.js
@@ -21,22 +21,11 @@ var server = http.createServer(function(request, response){
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-  if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    const filePath = path
+    response.write(fs.readFileSync(`./public/${filePath}`))
     response.end()
-  } else if(path === '/x'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
-    response.end()
-  } else {
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你输入的路径不存在对应的内容`)
-    response.end()
-  }
 
   /******** 代码结束，下面不要看 ************/
 })
